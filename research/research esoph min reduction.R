d7572327758f482e2c0b79bcf34a33156aacb18b
unknown <vladimir_ostrovski@web.de>
2025-02-14 11:31:10 +0100

    work on data

--- research/research esoph min reduction.R
+++ research/research esoph min reduction.R
@@ -77,10 +77,13 @@ write.results(res,"size_esoph_min_red_PT_1000.csv")
 ###########################################################
 
 # obtain minimum distance model for technical and simulate the test power
-mdr = min_dst_logit(frm,df,weights=df$n,test = asymptotic, alpha=0.05, nSimulation = 1000, fixIntercept = TRUE)
+mdr = min_dst_logit(frm,df,weights=df$n,test = asymptotic, alpha=0.05, nSimulation = 200, fixIntercept = FALSE)
 
-bndPoints=generateBoundaryPoints()
-res= simulatePowerAtBoundary(p=mdr$fitted,mdr, nSimulation=1000, eps=0.013)
+bndPoints=generateBoundaryPoints(p=df$p,mdr, nSimulation=1000, eps=0.013)
+saveRDS(bndPoints,"bndPointsEsophMinRed.rds")
+bndPoints=readRDS("bndPointsEsophMinRed.rds")
+
+res= simulatePowerAtBoundary(bndPoints)
 write.csv(res,"power_esoph_min_red_013.csv")
 
 
